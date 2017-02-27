ce3e0d819f47afc948426f3bc9714691eba48c79
siriusyct <jizhai_zl@192.168.3.22>
2017-02-28 06:29:07 +0800

    create

---
+++ shsytest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  shsytest
+//
+//  Created by jizhai_zl on 2017/2/27.
+//  Copyright © 2017年 zl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
